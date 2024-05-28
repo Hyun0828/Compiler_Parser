9cd4e21b4f192464734403ea58f7dbf9e6e452e6
dlwlsrud815 <dlwlsrud815@gmail.com>
2024-05-28 15:55:19 +0900

    fix : 입실론 처리

--- parser.py
+++ parser.py
@@ -61,13 +61,13 @@ for index, row_dict in enumerate(parsing_table):
 # input token
 def get_input_token():
     if len(sys.argv) != 2:
-        print("Usage: python test.py <filename>")
+        print("Usage: python parser.py <filename>")
         sys.exit(1)
 
     filename = sys.argv[1]
 
     try:
-        with open(filename, 'r') as file:
+        with open(filename, 'r', encoding='utf-8', errors='replace') as file: # 입실론 인코딩
             content = file.read().split()
 
     except FileNotFoundError:
@@ -92,6 +92,8 @@ class SLRParser:
     def parsing(self):
         while self.right_sub_string:
             print("----------")
+            print("left sub string : "+str(self.left_sub_string))
+            print("right sub string : "+str(self.right_sub_string))
             token = self.right_sub_string[0]
             current_state = self.state_stack[-1]
             print("current state : " + str(int(current_state)))
@@ -101,7 +103,7 @@ class SLRParser:
                 print("reject")
                 break
 
-            action_state = self.parsing_table[int(current_state)][str(token)] #s2, r5
+            action_state = self.parsing_table[int(current_state)][str(token)]  # s2, r5
             if action_state == "acc":
                 print("accept")
                 break
@@ -111,7 +113,7 @@ class SLRParser:
 
             print("action : " + action + str(new_state))
 
-            if action == 's':   # shift
+            if action == 's':  # shift
                 self.left_sub_string.append(self.right_sub_string.popleft())  # 오른쪽 첫번째 원소 팝 해서 왼쪽에 추가
                 self.state_stack.append(new_state)  # state 추가
                 print('token : ' + str(token))
@@ -133,14 +135,16 @@ class SLRParser:
                 new_node.children = children[::-1]
                 self.node_stack.append(new_node)
                 print('current state : ' + str(int(self.state_stack[-1])))
-                goto_state = self.parsing_table[self.state_stack[-1]][lhs]
+                goto_state = self.parsing_table[int(self.state_stack[-1])][lhs]
+                print(lhs)
                 self.state_stack.append(goto_state)  # goto
             # todo : error
-        return self.node_stack[-1] # 파싱 성공한 경우에 트리가 제대로 나옴
+        return self.node_stack[-1]  # 파싱 성공한 경우에 트리가 제대로 나옴
 
 
 if __name__ == '__main__':
     right_sub_string = deque(get_input_token())
+    right_sub_string.append('$') # 이거 있어야 파싱 끝까지 함
     parser = SLRParser(parsing_table, cfg, right_sub_string)
     root = parser.parsing()
     for pre, fill, node in RenderTree(root):
